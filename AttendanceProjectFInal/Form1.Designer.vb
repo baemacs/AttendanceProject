6606116bee4c78ab2023eefb693d4d9ab1188ec4
baemacs <mackristan@gmail.com>
2023-04-27 16:31:52 +0800

    first commit

---
+++ AttendanceProjectFInal/Form1.Designer.vb
@@ -0,0 +1,431 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.chkteacher = New System.Windows.Forms.CheckBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblmins = New System.Windows.Forms.Label()
+        Me.lblsecs = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.chkstu1 = New System.Windows.Forms.CheckBox()
+        Me.chkstu2 = New System.Windows.Forms.CheckBox()
+        Me.chkstu3 = New System.Windows.Forms.CheckBox()
+        Me.chkstu4 = New System.Windows.Forms.CheckBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel2.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel3.SuspendLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel4.SuspendLayout()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel5.SuspendLayout()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'chkteacher
+        '
+        Me.chkteacher.AutoSize = True
+        Me.chkteacher.Location = New System.Drawing.Point(21, 370)
+        Me.chkteacher.Name = "chkteacher"
+        Me.chkteacher.Size = New System.Drawing.Size(192, 21)
+        Me.chkteacher.TabIndex = 0
+        Me.chkteacher.Text = "Check to start attendance"
+        Me.chkteacher.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(435, 612)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(345, 85)
+        Me.TextBox1.TabIndex = 4
+        Me.TextBox1.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(436, 60)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(274, 32)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Class Lapsed Time"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(671, 124)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 17)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Secs"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(432, 124)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(37, 17)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Mins"
+        '
+        'lblmins
+        '
+        Me.lblmins.AutoSize = True
+        Me.lblmins.Location = New System.Drawing.Point(432, 155)
+        Me.lblmins.Name = "lblmins"
+        Me.lblmins.Size = New System.Drawing.Size(0, 17)
+        Me.lblmins.TabIndex = 8
+        '
+        'lblsecs
+        '
+        Me.lblsecs.AutoSize = True
+        Me.lblsecs.Location = New System.Drawing.Point(671, 155)
+        Me.lblsecs.Name = "lblsecs"
+        Me.lblsecs.Size = New System.Drawing.Size(0, 17)
+        Me.lblsecs.TabIndex = 9
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 2000
+        '
+        'Timer3
+        '
+        Me.Timer3.Interval = 1000
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM3"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.chkstu1)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Location = New System.Drawing.Point(935, 60)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(216, 260)
+        Me.Panel1.TabIndex = 10
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(22, 176)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(168, 25)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Mac Kristan Isaac"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.AttendanceProjectFInal.My.Resources.Resources.profile1
+        Me.PictureBox1.Location = New System.Drawing.Point(18, 15)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(182, 151)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 11
+        Me.PictureBox1.TabStop = False
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.chkstu2)
+        Me.Panel2.Controls.Add(Me.Label6)
+        Me.Panel2.Controls.Add(Me.PictureBox2)
+        Me.Panel2.Location = New System.Drawing.Point(1246, 60)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(216, 260)
+        Me.Panel2.TabIndex = 12
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(38, 180)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(141, 25)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Cha Eun Woo "
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.AttendanceProjectFInal.My.Resources.Resources.profile2
+        Me.PictureBox2.Location = New System.Drawing.Point(18, 21)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(182, 145)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 11
+        Me.PictureBox2.TabStop = False
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel3.Controls.Add(Me.chkstu3)
+        Me.Panel3.Controls.Add(Me.Label8)
+        Me.Panel3.Controls.Add(Me.PictureBox3)
+        Me.Panel3.Location = New System.Drawing.Point(935, 398)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(216, 260)
+        Me.Panel3.TabIndex = 12
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(47, 180)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(126, 25)
+        Me.Label8.TabIndex = 11
+        Me.Label8.Text = "Kim Da-hyun"
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.AttendanceProjectFInal.My.Resources.Resources.profile3
+        Me.PictureBox3.Location = New System.Drawing.Point(18, 21)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(182, 145)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 11
+        Me.PictureBox3.TabStop = False
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel4.Controls.Add(Me.chkstu4)
+        Me.Panel4.Controls.Add(Me.Label10)
+        Me.Panel4.Controls.Add(Me.PictureBox4)
+        Me.Panel4.Location = New System.Drawing.Point(1246, 398)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(216, 260)
+        Me.Panel4.TabIndex = 13
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(13, 180)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(185, 25)
+        Me.Label10.TabIndex = 11
+        Me.Label10.Text = "Mae Christine Isaac"
+        Me.Label10.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.AttendanceProjectFInal.My.Resources.Resources.profile4
+        Me.PictureBox4.Location = New System.Drawing.Point(18, 21)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(182, 145)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 11
+        Me.PictureBox4.TabStop = False
+        '
+        'Panel5
+        '
+        Me.Panel5.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel5.Controls.Add(Me.Label5)
+        Me.Panel5.Controls.Add(Me.Label7)
+        Me.Panel5.Controls.Add(Me.PictureBox5)
+        Me.Panel5.Location = New System.Drawing.Point(12, 104)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(216, 260)
+        Me.Panel5.TabIndex = 12
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(62, 197)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(83, 18)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Professor"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(22, 176)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(0, 25)
+        Me.Label7.TabIndex = 11
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = Global.AttendanceProjectFInal.My.Resources.Resources.profile5
+        Me.PictureBox5.Location = New System.Drawing.Point(18, 15)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(182, 151)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 11
+        Me.PictureBox5.TabStop = False
+        '
+        'chkstu1
+        '
+        Me.chkstu1.AutoSize = True
+        Me.chkstu1.Location = New System.Drawing.Point(27, 220)
+        Me.chkstu1.Name = "chkstu1"
+        Me.chkstu1.Size = New System.Drawing.Size(18, 17)
+        Me.chkstu1.TabIndex = 12
+        Me.chkstu1.UseVisualStyleBackColor = True
+        '
+        'chkstu2
+        '
+        Me.chkstu2.AutoSize = True
+        Me.chkstu2.Location = New System.Drawing.Point(18, 220)
+        Me.chkstu2.Name = "chkstu2"
+        Me.chkstu2.Size = New System.Drawing.Size(18, 17)
+        Me.chkstu2.TabIndex = 14
+        Me.chkstu2.UseVisualStyleBackColor = True
+        '
+        'chkstu3
+        '
+        Me.chkstu3.AutoSize = True
+        Me.chkstu3.Location = New System.Drawing.Point(18, 217)
+        Me.chkstu3.Name = "chkstu3"
+        Me.chkstu3.Size = New System.Drawing.Size(18, 17)
+        Me.chkstu3.TabIndex = 14
+        Me.chkstu3.UseVisualStyleBackColor = True
+        '
+        'chkstu4
+        '
+        Me.chkstu4.AutoSize = True
+        Me.chkstu4.Location = New System.Drawing.Point(18, 217)
+        Me.chkstu4.Name = "chkstu4"
+        Me.chkstu4.Size = New System.Drawing.Size(18, 17)
+        Me.chkstu4.TabIndex = 14
+        Me.chkstu4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1505, 745)
+        Me.Controls.Add(Me.Panel5)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.lblsecs)
+        Me.Controls.Add(Me.lblmins)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.chkteacher)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Attendance System"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel4.PerformLayout()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel5.ResumeLayout(False)
+        Me.Panel5.PerformLayout()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents chkteacher As CheckBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblmins As Label
+    Friend WithEvents lblsecs As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label4 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label6 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Label8 As Label
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Label10 As Label
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents chkstu1 As CheckBox
+    Friend WithEvents chkstu2 As CheckBox
+    Friend WithEvents chkstu3 As CheckBox
+    Friend WithEvents chkstu4 As CheckBox
+End Class
---
+++ AttendanceProjectFInal/Form1.vb
@@ -0,0 +1,184 @@
+﻿Imports System.IO
+Imports System.IO.Ports
+
+Public Class Form1
+
+    Dim secs As Integer
+    Dim mins As Integer
+
+    ' teacher flag 
+    Dim tflag As Integer
+
+    'flags for students 
+    Dim sflag1 As Integer ' student1 flag
+    Dim sflag2 As Integer 'student2 flag 
+    Dim sflag3 As Integer
+    Dim sflag4 As Integer
+
+
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        tflag = 0
+        sflag1 = 0
+        sflag2 = 0
+        sflag3 = 0
+        sflag4 = 0
+
+
+        SerialPort1.Close()
+        SerialPort1.PortName = "com3"
+        SerialPort1.BaudRate = "9600" 'use same baud rate as used in Arduino
+        SerialPort1.DataBits = 8
+        SerialPort1.Parity = Parity.None
+        SerialPort1.StopBits = StopBits.One
+        SerialPort1.Handshake = Handshake.None
+        SerialPort1.Encoding = System.Text.Encoding.Default
+        SerialPort1.Open()
+    End Sub
+
+
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        ' if the students comes in time, within 2 mints then the students will be marked present. 
+        If InStr(TextBox1.Text, "stu1") And sflag1 = 0 And tflag = 1 And secs <= 30 Then
+            chkstu1.Checked = True
+            sflag1 = 1
+            chkstu1.Text = "Present"
+        End If
+
+        If InStr(TextBox1.Text, "stu2") And sflag2 = 0 And tflag = 1 And secs <= 30 Then
+            chkstu2.Checked = True
+            sflag2 = 1
+            chkstu2.Text = "Present"
+        End If
+
+        If InStr(TextBox1.Text, "stu3") And sflag3 = 0 And tflag = 1 And secs <= 30 Then
+            chkstu3.Checked = True
+            sflag3 = 1
+            chkstu3.Text = "Present"
+        End If
+
+        If InStr(TextBox1.Text, "stu4") And sflag4 = 0 And tflag = 1 And secs <= 30 Then
+            chkstu4.Checked = True
+            sflag4 = 1
+            chkstu4.Text = "Present"
+        End If
+
+
+
+
+
+        ' the following conditions will be used to check if the students are late.
+
+        If InStr(TextBox1.Text, "stu1") And (sflag1 = 0) And tflag = 1 And secs > 30 And secs < 60 Then
+            chkstu1.Checked = True
+            sflag1 = 1
+            chkstu1.Text = "late"
+        End If
+
+        If InStr(TextBox1.Text, "stu2") And (sflag2 = 0) And tflag = 1 And secs > 30 And secs < 60 Then
+            chkstu2.Checked = True
+            sflag2 = 1
+            chkstu2.Text = "late"
+        End If
+
+        If InStr(TextBox1.Text, "stu3") And (sflag3 = 0) And tflag = 1 And secs > 30 And secs < 60 Then
+            chkstu3.Checked = True
+            sflag3 = 1
+            chkstu3.Text = "late"
+        End If
+
+        If InStr(TextBox1.Text, "stu4") And (sflag4 = 0) And tflag = 1 And secs > 30 And secs < 60 Then
+            chkstu4.Checked = True
+            sflag4 = 1
+            chkstu4.Text = "late"
+        End If
+
+
+
+        ' the following conditions will be used to check if the students are absent.
+
+        If (sflag1 = 0) And tflag = 1 And mins >= 1 Then
+
+            sflag1 = 1
+            chkstu1.Text = "absent"
+        End If
+
+        If (sflag2 = 0) And tflag = 1 And mins >= 1 Then
+
+            sflag2 = 1
+            chkstu2.Text = "absent"
+        End If
+
+        If (sflag3 = 0) And tflag = 1 And mins >= 1 Then
+
+            sflag3 = 1
+            chkstu3.Text = "absent"
+        End If
+
+
+        If (sflag4 = 0) And tflag = 1 And mins >= 1 Then
+
+            sflag4 = 1
+            chkstu4.Text = "absent"
+        End If
+
+
+    End Sub
+
+    Private Sub DataReceived(ByVal sender As Object, ByVal e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        Try
+
+            Dim mydata As String
+            mydata = SerialPort1.ReadExisting()
+
+            If TextBox1.InvokeRequired Then
+                TextBox1.Invoke(DirectCast(Sub() TextBox1.Text &= mydata, MethodInvoker))
+            Else
+                TextBox1.Text &= mydata
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+
+    Private Sub Timer2_Tick(sender As System.Object, e As System.EventArgs) Handles Timer2.Tick
+        'timer2 will be used to empty the textbox1 for the new data. the timer interval is set to 1 second. 
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Timer3_Tick(sender As System.Object, e As System.EventArgs) Handles Timer3.Tick
+        ' timer3 will be used for the elapsed time. and this timer will be activated only when the teacher will check the box.
+        secs = secs + 1
+        lblsecs.Text = secs
+        lblmins.Text = mins
+        If secs = 60 Then
+            secs = 0
+            mins = mins + 1
+        End If
+
+    End Sub
+
+    Private Sub chkteacher_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles chkteacher.CheckedChanged
+        If chkteacher.Enabled = True Then
+            tflag = 1
+            Timer3.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles chkstu1.CheckedChanged
+
+    End Sub
+End Class
